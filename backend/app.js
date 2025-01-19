c6c04d6154ad239b3a4e46f10e5b6474c19708e3
Abdelrahman Elkhateeb <109158103+abdelrahman-elkhateeb@users.noreply.github.com>
2025-01-19 15:54:26 +0200

    setup user route and authController

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const userRouter = require("./routes/userRoute")
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controller/errorController");
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/api/users', userRouter);
 
 app.all("*", (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
---
+++ backend/controller/authController.js
@@ -0,0 +1,33 @@
+const User = require('./../models/userModel');
+const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+
+exports.signup = catchAsync(async (req, res, next) => {
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+    userType: req.body.userType
+  });
+
+  res.status(201).json({ status: 'success', data: newUser });
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // 1) Check if email and password exist
+  if (!email || !password) {
+    return next(new AppError('Please provide email and password', 400));
+  }
+  // 2) Check if password is correct and the user exist
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!(await user.correctPassword(password, user.password)) || !user)
+    return next(new AppError('Incorrect email or password', 401));
+
+  res.status(200).json({
+    status: 'success',
+  });
+});
