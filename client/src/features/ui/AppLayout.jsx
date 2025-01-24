56204b76cfa1b933651da3c3307224873da77408
Abdelrahman Elkhateeb <109158103+abdelrahman-elkhateeb@users.noreply.github.com>
2025-01-24 15:09:51 +0200

    remove the navbar from the registration pages

--- client/src/features/ui/AppLayout.jsx
+++ client/src/features/ui/AppLayout.jsx
@@ -1,10 +1,18 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "./Navbar";
 
 function AppLayout() {
+  const location = useLocation(); // Get the current route
+
+  // Define routes where the Navbar should NOT be rendered
+  const hideNavbarRoutes = ["/login", "/signup"];
+
+  // Check if the current route is in the hideNavbarRoutes array
+  const shouldShowNavbar = !hideNavbarRoutes.includes(location.pathname);
+
   return (
     <>
-      <Navbar />
+      {shouldShowNavbar && <Navbar />}
       <section>
         <Outlet />
       </section>
