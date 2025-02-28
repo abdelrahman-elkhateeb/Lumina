060d6c7c46176026fd9a7cf4c23b84cdedad91f0
Abdelrahman Elkhateeb <109158103+abdelrahman-elkhateeb@users.noreply.github.com>
2025-02-28 19:26:13 +0200

    Update courseRoute.js

--- backend/src/course/courseRoute.js
+++ backend/src/course/courseRoute.js
@@ -6,4 +6,8 @@ const router = express.Router();
 
 router.post("/createCourse", authController.protect, authController.restrictTo("instructor"), courseController.createCourse);
 
+router.post("/createSection", authController.protect, authController.restrictTo("instructor"), courseController.createSection);
+
+router.post("/createLesson", authController.protect, authController.restrictTo("instructor"), courseController.createLesson);
+
 module.exports = router;
