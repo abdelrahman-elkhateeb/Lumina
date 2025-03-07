d82546e910aaa4f8876a77180f35d09a7fd0e273
Abdelrahman Elkhateeb <109158103+abdelrahman-elkhateeb@users.noreply.github.com>
2025-03-07 17:56:56 +0200

    Update HeroSection.jsx

--- client/src/app/features/home/HeroSection.jsx
+++ client/src/app/features/home/HeroSection.jsx
@@ -3,34 +3,63 @@ import { motion } from "framer-motion";
 
 function heroSection() {
   return (
-    <div className="flex items-center flex-col justify-center h-dvh text-center relative z-50">
+    <div className="flex items-center flex-col justify-center h-[calc(100vh-78px)] text-center relative z-50">
+      <svg
+        width="150"
+        height="150"
+        viewBox="0 0 200 200"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        {/* Bulb Base */}
+        <rect x="90" y="140" width="20" height="30" fill="#555" />
+        <rect x="85" y="160" width="30" height="10" fill="#777" />
+
+        {/* Light Bulb (Flicker Effect) */}
+        <circle
+          cx="100"
+          cy="100"
+          r="40"
+          fill="yellow"
+          className="animate-flicker"
+        />
+
+        {/* Glow Effect */}
+        <circle
+          cx="100"
+          cy="100"
+          r="50"
+          fill="grey"
+          className="opacity-10"
+        />
+      </svg>
       <motion.h1
-        className="text-7xl"
+        className="text-3xl md:text-7xl"
         initial={{ opacity: 0, y: 50 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.8, ease: "easeOut" }}
       >
         Embark on a Learning Odyssey – Explore the Universe of Code!
       </motion.h1>
+
       <div className="flex justify-center items-center flex-col">
         <motion.h4
-          className="text-2xl text-accent mt-4"
+          className="text-xl md:text-2xl text-accent mt-4"
           initial={{ opacity: 0, y: 30 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.8, ease: "easeOut", delay: 0.3 }}
         >
           Chart your course through personalized lessons, hands-on coding, and cosmic projects to elevate your skills.
         </motion.h4>
-        <motion.h4
-          className="w-fit mt-4 p-3 rounded-lg bg-primary text-secondary font-bold hover:bg-accent transition"
+
+        <motion.button
           initial={{ opacity: 0, y: 30 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.8, ease: "easeOut", delay: 0.3 }}
+          className="mt-7"
         >
-          <button>
-            <Link to="/courses">Get Started</Link>
-          </button>
-        </motion.h4>
+          <Link to="/courses" className="bg-site-accent font-light p-4 rounded-lg">Get Started</Link>
+        </motion.button>
       </div>
     </div>
   )
