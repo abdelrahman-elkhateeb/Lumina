d05de9d48d84c7555ee8e6005927e2ce67758622
Abdelrahman Elkhateeb <109158103+abdelrahman-elkhateeb@users.noreply.github.com>
2025-03-16 04:08:28 +0200

    Update MyCourses.jsx

--- client/src/app/features/course/myLearning/MyCourses.jsx
+++ client/src/app/features/course/myLearning/MyCourses.jsx
@@ -12,21 +12,37 @@ function MyCourses() {
   if (error) return <p>Error loading courses.</p>;
 
   return (
-    <section className="min-h-dvh container mx-auto px-4">
+    <section className="min-h-dvh container mx-auto px-4 py-8">
       <Heading img={spaceMan} title="your learnings" />
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
         {courses.map((course) => (
           <Link to={`/courses/${course._id}`} key={course._id}>
-            <div>
-              <img src={course.courseImage} alt="course image" />
-              <h2 className="text-lg font-bold">{course.title}</h2>
-              <p>{course.instructor.name}</p>
-              <p>{course.category}</p>
+            <div className="bg-white dark:bg-gray-800 rounded-xl shadow-md hover:shadow-lg transition duration-300 overflow-hidden hover:scale-105 transform relative">
+              <img
+                src={course.courseImage}
+                alt={`${course.title} image`}
+                className="w-full"
+              />
+              <div className="p-4 space-y-2 absolute z-10 left-0 bottom-0 bg-site-secondary">
+                <h2 className="text-xl font-bold text-primary capitalize">
+                  {course.title}
+                </h2>
+                <p className="text-sm text-site-text capitalize">
+                  Instructor:{" "}
+                  <span className="font-medium ">
+                    {course.instructor.name}
+                  </span>
+                </p>
+                <span className="inline-block bg-site-accent text-xs px-3 py-1 rounded-full">
+                  {course.category}
+                </span>
+              </div>
             </div>
           </Link>
         ))}
       </div>
     </section>
+
   );
 }
 
