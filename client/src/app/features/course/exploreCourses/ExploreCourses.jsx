2587a65a94f2a64ca894332ee5fc9cb7fe32be86
Abdelrahman Elkhateeb <109158103+abdelrahman-elkhateeb@users.noreply.github.com>
2025-03-20 16:16:27 +0200

    Update ExploreCourses.jsx

--- client/src/app/features/course/exploreCourses/ExploreCourses.jsx
+++ client/src/app/features/course/exploreCourses/ExploreCourses.jsx
@@ -1,12 +1,17 @@
 import Heading from "../../ui/Heading";
-import spaceMan from "../../../../../public/assets/spaceMan(1).svg";
+import spaceMan from "../../../../../public/assets/spaceMan(4).svg";
+import { useGetcoursesPreviewQuery } from "../../redux/courses/coursesApi";
 
 function exploreCourses() {
+  const { data, error, isLoading } = useGetcoursesPreviewQuery();
+  console.log(data);
+  
   return (
     <section className="bg-background-500 min-h-dvh container mx-auto px-4">
       <Heading img={spaceMan} title="explore the magic of our courses" />
+
       <div>
-        
+    
       </div>
     </section>
   )
