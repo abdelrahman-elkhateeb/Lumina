460600cb6db6180c199c148c80f6c3902ec0c89d
Abdelrahman Elkhateeb <109158103+abdelrahman-elkhateeb@users.noreply.github.com>
2025-03-20 15:07:17 +0200

    Update ExploreCourses.jsx

--- client/src/app/features/course/exploreCourses/ExploreCourses.jsx
+++ client/src/app/features/course/exploreCourses/ExploreCourses.jsx
@@ -1,7 +1,13 @@
+import Heading from "../../ui/Heading";
+import spaceMan from "../../../../../public/assets/spaceMan(1).svg";
+
 function exploreCourses() {
   return (
-    <section className="bg-background-500 min-h-dvh">
-
+    <section className="bg-background-500 min-h-dvh container mx-auto px-4">
+      <Heading img={spaceMan} title="explore the magic of our courses" />
+      <div>
+        
+      </div>
     </section>
   )
 }
