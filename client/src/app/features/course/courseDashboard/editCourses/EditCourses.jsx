2d2a806030ec98ca05b5ddfcd68f04aeb3d1c519
Abdelrahman Elkhateeb <109158103+abdelrahman-elkhateeb@users.noreply.github.com>
2025-03-31 16:24:20 +0200

    Update EditCourses.jsx

--- client/src/app/features/course/courseDashboard/editCourses/EditCourses.jsx
+++ client/src/app/features/course/courseDashboard/editCourses/EditCourses.jsx
@@ -1,6 +1,8 @@
 import { useInstructorCoursesQuery } from "../../../redux/courses/coursesApi";
 import CourseCard from "./CourseCard";
 import LightBulbLoader from "../../../ui/LightBulbLoader";
+import Heading from "../../../ui/Heading";
+import spaceMan from "../../../../../../public/assets/spaceMan(7).svg";
 
 function EditCourses() {
   const { data, isLoading, error, refetch } = useInstructorCoursesQuery();
@@ -11,6 +13,7 @@ function EditCourses() {
 
   return (
     <section className="container mx-auto px-4">
+      <Heading title="manage all your content" img={spaceMan} />
       <CourseCard courses={data?.courses} refetch={refetch} />
     </section>
   )
