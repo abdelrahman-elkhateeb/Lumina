98108d2eca25d1fb9ed037adeb4197fe49a0c153
Abdelrahman Elkhateeb <109158103+abdelrahman-elkhateeb@users.noreply.github.com>
2025-03-31 23:53:03 +0200

    start working on edit  course page

--- client/src/app/features/course/courseDashboard/editCourses/EditCoursePage.jsx
+++ client/src/app/features/course/courseDashboard/editCourses/EditCoursePage.jsx
@@ -1,9 +1,13 @@
+import Heading from "../../../ui/Heading";
+import spaceman from "../../../../../../public/assets/spaceMan(1).svg";
+
 function EditCoursePage() {
   return (
     <div>
+      <Heading title="edit your content here" img={spaceman} />
       
     </div>
   )
 }
 
-export default EditCoursePage
+export default EditCoursePage;
