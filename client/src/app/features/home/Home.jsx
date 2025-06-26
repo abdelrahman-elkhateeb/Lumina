a6bd861088bea3f824a40e54943d4d08905f807e
Abdelrahman Elkhateeb <109158103+abdelrahman-elkhateeb@users.noreply.github.com>
2025-06-26 20:24:56 +0300

    delete recommended section

--- client/src/app/features/home/Home.jsx
+++ client/src/app/features/home/Home.jsx
@@ -1,12 +1,10 @@
 import FeaturesSection from './FeaturesSection';
 import HeroSection from './HeroSection';
-import Recommended from './Recommended';
 
 function Home() {
   return (
     <section className="container m-auto px-4">
       <HeroSection />
-      {/* <Recommended /> */}
       <FeaturesSection />
     </section>
   )
